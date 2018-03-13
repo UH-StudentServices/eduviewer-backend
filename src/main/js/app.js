b79985518363e61f5ac98c25ac48c5f2fbc1ddc9
hprajani <hannu-pekka.rajaniemi@helsinki.fi>
2018-03-13 20:26:08 +0200

    dropdowns now have names instead of ids and support multiselect

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -341,8 +341,18 @@ class Dropdown extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {selectedId: []};
+        this.state = {elements: [], selected: {}};
         this.onChange = this.onChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('/api/all_ids?lv=' + this.props.lv, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(this.props.ids)
+        }).then((response) => response.json()).then(responseJson => {
+            this.setState({elements: responseJson});
+        });
     }
 
     onChange(event) {
@@ -351,23 +361,48 @@ class Dropdown extends React.Component {
 
 
     render() {
-        var options = this.props.ids.map(id =>
-            <option key={id} value={id}>{id}</option>
+        if(this.state.elements.length == 0) {
+            return (<div>...</div>);
+        }
+        var options = this.state.elements.map(element =>
+            <option key={element.id} value={element.id}>{element.name.fi}</option>
         );
+
+        var selected = getSelectValues(document.getElementById("select-" + this.props.id));
 
         return (
             <div>
-                <select onChange={this.onChange}>
-                    <option value="">---</option>
+                <select key="test" id={'select-' + this.props.id} onChange={this.onChange}>
+                    <option key="" value="">---</option>
                     {options}
                 </select>
                 <br/>
                 <ElementList key={'mods-' + this.props.rule.localId} id={'mods-' + this.props.rule.localId}
-                             ids={this.state.selectedId} lv={this.props.lv} rule={this.props.rule}/>
+                             ids={selected} lv={this.props.lv} rule={this.props.rule}/>
             </div>
 
         )
     }
+}
+
+function getSelectValues(select) {
+    var result = [];
+    var options = select && select.options;
+    if(options == null) {
+        return [];
+    }
+    var opt;
+
+    for (var i=0; i < options.length; i++) {
+        opt = options[i];
+
+        if (opt.selected) {
+            console.log("opt: ");
+            console.log(opt);
+            result.push(opt.value);
+        }
+    }
+    return result;
 }
 
 
