7e8b1d1a40dfb526355b14d983d0d12d0e7f9063
eero3 <eero.siren@kolumbus.fi>
2018-10-24 16:44:32 +0300

    fixed checkstyle errors in Config and EduviewerApplication

--- src/main/java/fi/helsinki/eduview/Config.java
+++ src/main/java/fi/helsinki/eduview/Config.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Eduviewer application.
+ *
+ * Eduviewer application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Eduviewer application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Eduviewer application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fi.helsinki.eduview;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +26,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * @author: Hannu-Pekka Rajaniemi (h-p@iki.fi)
- * @date: 20/02/2018
- */
 @Configuration
 @PropertySource("file:${ext.properties}")
 @EnableScheduling
