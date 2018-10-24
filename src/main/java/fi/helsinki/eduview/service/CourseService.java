45b7a13f17ecfa2b6469cc9b484ec6c037decbc7
eero3 <eero.siren@kolumbus.fi>
2018-10-24 16:45:13 +0300

    fixed checkstyle errors in CourseService and removed unused init boolean

--- src/main/java/fi/helsinki/eduview/service/CourseService.java
+++ src/main/java/fi/helsinki/eduview/service/CourseService.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Eduviewer application.
+ *
+ * Eduviewer application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Eduviewer application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Eduviewer application.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fi.helsinki.eduview.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -6,46 +23,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author: Hannu-Pekka Rajaniemi (h-p@iki.fi)
- * @date: 14/01/2018
- */
 @Service
 public class CourseService extends AbstractDataService {
 
     private Logger logger = LogManager.getLogger(CourseService.class);
 
-    private boolean init = false;
     private ObjectMapper mapper = new ObjectMapper();
     private List<JsonNode> cus = new ArrayList<>();
-
-
-    @PostConstruct
-    private void init() throws IOException {
-//        if(init) {
-//            return;
-//        }
-//        ObjectMapper mapper = new ObjectMapper();
-//        List<JsonNode> root = cus;
-//        String courseUnitsFileName = env.getProperty("course-units", "course-units");
-//        for(File file : new File(env.getProperty("data-location", "backup/")).listFiles()) {
-//            if(!file.getName().contains(courseUnitsFileName)) {
-//                continue;
-//            }
-//            ArrayNode fileTree = (ArrayNode)mapper.readTree(Files.readAllBytes(file.toPath()));
-//            for(JsonNode child : fileTree) {
-//                root.add(child);
-//            }
-//        }
-        init = true;
-    }
 
     protected JsonNode find(String id) throws Exception {
         return openFile(id);
@@ -53,7 +43,7 @@ public class CourseService extends AbstractDataService {
 
     private JsonNode openFile(String id) throws IOException {
         File file = new File(env.getProperty("data-location") + env.getProperty("course-units-dir") + "/" + id + ".json");
-        if(file.isFile()) {
+        if (file.isFile()) {
             return mapper.readTree(Files.readAllBytes(file.toPath()));
         }
         return mapper.createArrayNode();
